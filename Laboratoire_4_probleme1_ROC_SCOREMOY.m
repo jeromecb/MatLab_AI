e3a8f9f4e339c1ee1181490831586ca8654cf4af
jeromecb <combetbj@gmail.com>
2016-11-06 15:55:31 -0500

    Creation de dossier pour chaque probleme

---
+++ Laboratoire_4_probleme1_ROC_SCOREMOY.m
@@ -0,0 +1,29 @@
+%% LAB 3
+clear all
+close all
+clc
+%% Charger LIBSVM
+addpath(genpath('libsvm-3.20\matlab\'))
+
+%% Charger la base de données
+load Prob1.mat
+
+%% Apprendre avec le SVM (le validation croisée n'est pas implémentée)
+nFolds = 10;
+f = 3;
+C = [0.01 0.1 1 10 100 1000];
+%C = 10000000000;
+gamma = [0.0001 0.001 0.01 0.1 1 10 100];
+%gamma = 0.001;
+kernel = 0;
+
+% séparer les données en folds
+[indexPerFold] = obtenirListeParFold(Ytrain,nFolds);
+
+%Calcul du temps de traitement
+accT = [];
+
+% obtenir les données d'entrainement et de validation selon
+% la sépration que l'on vient d'effectuer. 
+[XTR, YTR, XVAL, YVAL] = ObtenirDatasetsPourFold(Xtrain,Ytrain,indexPerFold,f);
+
