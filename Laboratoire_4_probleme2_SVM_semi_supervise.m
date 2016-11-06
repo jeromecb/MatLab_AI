5bb99b5ed6d073f9b2aa9bd52dc0a8a5fdbb2f74
jeromecb <combetbj@gmail.com>
2016-11-06 16:07:16 -0500

    Modification SVM semi supervisé

--- Laboratoire_4_probleme2_SVM_semi_supervise.m
+++ Laboratoire_4_probleme2_SVM_semi_supervise.m
@@ -15,7 +15,7 @@ C = [0.01 0.1 1 10 100 1000];
 
 gamma = [0.0001 0.001 0.01 0.1 1 10 100];
 
-kernel = 0;
+kernel = 2;
 
 % séparer les données en folds
 [indexPerFold] = obtenirListeParFold(Ytrain,nFolds);
@@ -27,3 +27,35 @@ accT = [];
 % la sépration que l'on vient d'effectuer. 
 [XTR, YTR, XVAL, YVAL] = ObtenirDatasetsPourFold(Xtrain,Ytrain,indexPerFold,f);
 
+%%Entrainement supervisé
+%Ici on va venir faire l'entrainement superviser de notre SVM
+
+for g=1:length(gamma)
+    for c=1:length(C)
+        for f=1:nFolds      
+            % créer un string contenant les paramètres pour configurer le SVM
+            % pout plus d'information taper svmtrain dans la console.
+            SVMstr = ['-t ' num2str(kernel) ' -c ' num2str(C(c)) ' -g ' ...
+                num2str(gamma(g)) ' -q'];
+            % obtenir les données d'entrainement et de validation selon
+            % la sépration que l'on vient d'effectuer.
+            [XTR, YTR, XVAL, YVAL] = ObtenirDatasetsPourFold(Xtrain,Ytrain,indexPerFold,f);
+            
+            % Entrainer le SVM sur la base d'entrainement
+            modelSVMGAUSS = svmtrain(YTR,XTR,SVMstr);
+            
+            % classifier la base de validation avec le SVM
+            [predY, accuracySVMGAUSS(f,:), DV] = svmpredict(YVAL, XVAL, modelSVMGAUSS, '-q');
+          
+        end
+        %Accumulation des taux en 3D gamma et C
+        accTauxGAUSS(g,c) = mean(accuracySVMGAUSS(:,1)); 
+    end
+end
+
+%%Comme on a pu le voir dans le papier de l'ÉTS ici on va venir libeler nos
+%%valeurs encore non libelé pour pouvoir apprendre avec ces valeurs par la
+%%suite
+
+accuracyGAUSS = max(accTauxGAUSS);
+[rowGAUSS colGAUSS] = find(accTauxGAUSS == max(max(accTauxGAUSS)));
